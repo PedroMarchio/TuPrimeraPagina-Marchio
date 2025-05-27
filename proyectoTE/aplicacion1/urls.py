456be76d3941c72fcc60ff24b5357cd004cc5182
PedroMarchio <142613511+PedroMarchio@users.noreply.github.com>
2025-05-27 18:49:42 -0300

    3.0 Tercera Entrega

--- proyectoTE/aplicacion1/urls.py
+++ proyectoTE/aplicacion1/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inicio/', views.inicio, name="inicio"),
-    path('bienvenida/', views.bienvenida, name="bienvenida"),
-    path('bienvenido/<nombre>/<apellido>/', views.bienvenido, name="bienvenido"),
-    path('bienvenido_tpl/', views.bienvenido_tpl, name="bienvenido_tpl"),
-    path('nueva_pagina/', views.nueva_pagina, name="nueva_pagina"),
+    path('inicio/', views.bienvenido_tpl, name="inicio"),
+    #path('bienvenida/', views.bienvenida, name="bienvenida"),
+    #path('bienvenido/<nombre>/<apellido>/', views.bienvenido, name="bienvenido"),
+    #path('bienvenido_tpl/', views.bienvenido_tpl, name="bienvenido_tpl"),
+    #path('nueva_pagina/', views.nueva_pagina, name="nueva_pagina"),
     path('clientes/', views.clientes_web, name="clientes"),
+    path('clientes_form/', views.clienteForm, name="clientes_form"),
+    path('productos/', views.productos_web, name="productos"),
+    path('productos_form/', views.productoForm, name="productos_form"),
+    path('vendedores/', views.vendedores_web, name="vendedores"),
+    path('vendedor_form/', views.vendedoresForm, name="vendedor_form"),
 ]
--- proyectoTE/aplicacion1/views.py
+++ proyectoTE/aplicacion1/views.py
@@ -4,7 +4,7 @@ import datetime, random
 from .models import *
 from .forms import *
 
-# Create your views here.
+# Create your views here. todo es practica que quedó jajjaa
 def inicio(request):
     inicio = "Bien venidos a Django"
     return HttpResponse(inicio)
@@ -40,10 +40,11 @@ def nueva_pagina(request):
     nombre = random.choices(["Pelota","Perro de Goma","Patito","Sapo","Tetera","Muñeca"], k=1)[0]
     precio = random.randint(100, 5001)
     cantidad = random.randint(100, 999)
-    producto = productos(nombre=nombre, precio=precio, cantidad=cantidad)
+    producto = Productos(nombre=nombre, precio=precio, cantidad=cantidad)
     producto.save()
     return render(request, "aplicacion1/nueva_pagina.html", {"nombre":nombre, "precio":precio, "cantidad":cantidad,})
 
+#--------------------- desde acá hice el TP
 def clientes_web(request):
     clientes = Clientes.objects.all()
     return render(request, "aplicacion1/clientes.html", {"clientes":clientes})
@@ -59,7 +60,52 @@ def clienteForm(request):
             ciudad =form.cleaned_data["ciudad"]
             cliente = Clientes(nombre=nombre, apellido=apellido, numeroContacto=numeroContacto, correo=correo, ciudad=ciudad)
             cliente.save()
-            return render(request, "aplicacion1/clientes.html", {"clientes": cliente})
+            clientes = Clientes.objects.all()
+            return render(request, "aplicacion1/clientes.html", {"clientes": clientes})
     else:
         form = clientesForm()
-    return render(request, "aplicacion1/curso_form.html", {"form": form})
+    return render(request, "aplicacion1/cliente_form.html", {"form": form})
+
+#---------------------
+def productos_web(request):
+    productos = Productos.objects.all()
+    return render(request, "aplicacion1/productos.html", {"productos":productos})
+
+def productoForm(request):
+    if request.method == "POST":
+        form = productosForm(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data["nombre"]
+            precio = form.cleaned_data["precio"]
+            cantidad = form.cleaned_data["cantidad"]
+            producto = Productos(nombre=nombre, precio=precio, cantidad=cantidad)
+            producto.save()
+            productos = Productos.objects.all()
+            return render(request, "aplicacion1/productos.html", {"productos": productos})
+    else:
+        form = productosForm()
+    return render(request, "aplicacion1/producto_form.html", {"form": form})
+
+#----------------------
+
+def vendedores_web(request):
+    vendedores = Vendedor.objects.all()
+    return render(request, "aplicacion1/vendedores.html", {"vendedores":vendedores})
+
+def vendedoresForm(request):
+    if request.method == "POST":
+        form = vendedorForm(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data["nombre"]
+            apellido = form.cleaned_data["apellido"]
+            numeroContacto = form.cleaned_data["numeroContacto"]
+            correo = form.cleaned_data["correo"]
+            ciudad =form.cleaned_data["ciudad"]
+            vendedor = Vendedor(nombre=nombre, apellido=apellido, numeroContacto=numeroContacto, correo=correo, ciudad=ciudad)
+            vendedor.save()
+            vendedores = Vendedor.objects.all()
+            return render(request, "aplicacion1/vendedores.html", {"vendedores": vendedores})
+    else:
+        form = vendedorForm()
+    return render(request, "aplicacion1/vendedor_form.html", {"form": form})
+
